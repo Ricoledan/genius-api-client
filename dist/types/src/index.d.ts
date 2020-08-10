8656c2fe6434b1ee4f36643f07e7a682fecdeed6
Ricardo <ricoledan@gmail.com>
2020-08-10 15:51:14 -0400

    feat: augment rollup

--- dist/types/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Search } from './types';
-declare const getArtistIdDetails: (name: string) => Promise<void | Search>;
-declare const getArtistDetails: (id: string) => void;
-declare const getSongDetails: (id: string) => void;
-export { getArtistIdDetails, getArtistDetails, getSongDetails };
--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,59 @@
 import resolve from '@rollup/plugin-node-resolve'
+import localResolve from 'rollup-plugin-local-resolve'
 import pkg from './package.json'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import filesize from 'rollup-plugin-filesize'
+import minify from 'rollup-plugin-babel-minify'
+import { terser } from 'rollup-plugin-terser'
 const extensions = ['.js', '.ts']
 
-export default [
+const INPUT_FILE_PATH = 'src/index.ts'
+
+const GLOBALS = {
+  axios: 'axios',
+}
+
+const EXTERNAL = ['axios']
+
+const OUTPUT_DATA = [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-      },
-    ],
-    plugins: [
-      json(),
-      commonjs(),
-      resolve({
-        extensions,
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        extensions,
-      }),
-    ],
+    file: pkg.main,
+    format: 'cjs',
+  },
+  {
+    file: pkg.module,
+    format: 'es',
   },
 ]
+
+const PLUGINS = [
+  json(),
+  localResolve(),
+  resolve({
+    extensions,
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    extensions,
+  }),
+  minify(),
+  terser(),
+  commonjs(),
+  filesize(),
+]
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+  input: INPUT_FILE_PATH,
+  output: {
+    file,
+    format,
+    name: pkg.name,
+    globals: GLOBALS,
+  },
+  external: EXTERNAL,
+  plugins: PLUGINS,
+}))
+
+export default config
